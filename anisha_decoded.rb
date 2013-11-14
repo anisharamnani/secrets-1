3b255bcc3922e967f2c7e4b041a271e3aa02ff0a
Flatiron <flatiron@new-host-25.home>
2013-11-14 17:58:54 -0500

    decoder for uzo

---
+++ anisha_decoded.rb
@@ -0,0 +1,21 @@
+gsub_reverse_secret = "21UfoUegaUehtUlitnuUdebUniUdeepUovatsuGUdnaUozU"
+
+reverse_secret = gsub_reverse_secret.gsub("U"," ")
+
+secret = reverse_secret.reverse 
+
+secret = "U" + secret
+
+puts secret 
+
+# secret = "Uzo and Gustavo peed in bed until the age of 12"
+
+# # It reverses the secret 
+# #printing it results: 1 fo ega eht litnu deb ni deep ovatsuG dna ozU
+# reverse_secret = secret.reverse
+
+# #This method substitute the white spaces with the last char of the secrets string reversed
+# #prints 1UfoUegaUehtUlitnuUdebUniUdeepUovatsuGUdnaUozU
+# gsub_reverse_secret = reverse_secret.gsub(" ", reverse_secret[-1])
+
+# puts gsub_reverse_secret
